ada51f1916f857a936b0a83d1ead1d8e82e981e3
EmanHass <eman_hassouna@hotmail.com>
2021-07-28 15:26:18 +0300

    fix link API & fix appendChild & create variable (innerDiv) andappend it into div which have class ('juice') relates #5

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
-const drinks = document.getElementsByClassName('drinks')[0];
+const drinks = document.querySelector('.drinks');
 
-fetch('www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
 .then((response) => {
     if (response.status !== 200) {
       console.log(
@@ -11,19 +11,29 @@ fetch('www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
       return response.json();      
     }
   })
-.then((data) =>{
-    for(let drink in data.drinks){
-        const div = document.createElement('div');
-        div.setAttribute('class','juice');
-        div.appendChild('drinks');
+  .then((data) => {
+    for (let r of data.drinks){
 
-        const img = document.createElement('img');
-        img.setAttribute('src',drink.strDrinkThumb);
-        div.appendChild('img');
+      const content = document.createElement("div");
+      content.setAttribute("class", "juice");
 
-        const id = document.createElement('p')
-        id.setAttribute('class','id');
-        id.textContent= 'ID: '+ drink.idDrink;
-        div.appendChild('id');
-    }
-})
+      const img = document.createElement('img');
+      img.setAttribute('src',`${r.strDrinkThumb})`);
+
+      const h1 = document.createElement("h1");
+      h1.textContent ='ID: '+ r.idDrink;
+      const p = document.createElement("p");
+      p.textContent = r.strInstructions; 
+
+      const innerDiv=document.createElement('div');
+      innerDiv.setAttribute('class','innerDiv');
+
+      innerDiv.appendChild(h1);
+      innerDiv.appendChild(p);
+
+      content.appendChild(img);
+      content.appendChild(innerDiv);
+      drinks.appendChild(content);
+
+    };
+  });
