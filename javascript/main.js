09eab5d1bf4e0801d2fb89f3c62476f30933b2b6
salsabeelomar <omarsalsabeel65@gmail.com>
2021-07-29 13:54:19 +0300

    add function that when the ueser click on the juice appear the instruction , add that drink is alcoholic or not add the name for the drink , complete catch relates #8

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
 const drinks = document.querySelector('.drinks');
 
-fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+fetch('https://www.thecocktaildb.com/api/pjson/v1/1/search.php?s=margarita')
 .then((response) => {
     if (response.status !== 200) {
       console.log(
@@ -19,21 +19,34 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
 
       const img = document.createElement('img');
       img.setAttribute('src',`${r.strDrinkThumb}`);
+      img.classList.add("images")
 
-      const h1 = document.createElement("h1");
+      const h1 = document.createElement("h3");
       h1.textContent ='ID: '+ r.idDrink;
-      const p = document.createElement("p");
-      p.textContent = r.strInstructions; 
-
+      
       const innerDiv=document.createElement('div');
       innerDiv.setAttribute('class','innerDiv');
 
+      const drinkName=document.createElement("h3");
+      drinkName.classList.add("drinkName")
+      drinkName.textContent=" Drink name :"+r.strDrink;
+
+      const alcoholic =document.createElement("h3")
+      alcoholic.textContent="Alcoholic :"+r.strAlcoholic
+
+
+      innerDiv.addEventListener("click",function(){
+      const p = document.createElement("p");
+      p.textContent ="Instructions :"+r.strInstructions;   
+       innerDiv.appendChild(p);
+      })
+      innerDiv.appendChild(drinkName);
       innerDiv.appendChild(h1);
-      innerDiv.appendChild(p);
+   innerDiv.appendChild(alcoholic)
 
       content.appendChild(img);
       content.appendChild(innerDiv);
       drinks.appendChild(content);
 
     };
-  });
+  }).catch((eror)=>console.log(eror))
